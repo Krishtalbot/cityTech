8356c5921d014aa39dec483450d8270131ed44b8
Krishtal <krishtalking33@gmail.com>
2025-03-21 16:02:34 +0545

    Minor loop changes

--- inversion.py
+++ inversion.py
@@ -1,7 +1,7 @@
 def inversion(arr):
     count = 0
     inversions = []
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         for j in range(i + 1, len(arr)):
             if arr[i] > arr[j]:
                 count += 1
